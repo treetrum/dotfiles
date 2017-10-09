1d51d31ec64f8f0d82fa9278da3f2b022ff2c8d3
TreeTrum <sam@sjd.co>
2017-10-09 12:26:03 +1100

    Better file structure and installing logic

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,35 +6,53 @@
 
 ######### Variables
 
-dir=~/dotfiles			# dotfiles directory
-olddir=~/dotfiles_old		# old dotfiles backup directory
-files="vimrc eslintrc.yml sass-lint.yml"			# list of files/folders to symlink in homeidr
-colorsdir=~/dotfiles/vimcolors
+dir=$(pwd)			# dotfiles directory
+backupdir="$dir/backup"		# old dotfiles backup directory
+colorsdir="$dir/vimcolors"
+dotfilesdir="$dir/dotfiles"
+
+files="eslintrc.yml sass-lint.yml vimrc"			# list of files/folders to symlink in homeidr
 colors="itg_flat.vim spacegray.vim"
 
 ########
 
-# Create dotfiles_old in homdir
-echo "Creating $olddir for backup of any existing dotfiles in ~/"
-mkdir -p $olddir
-echo "...done"
-
-# Change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+# Create create backup dir
+mkdir -p $backupdir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+echo ""
+echo "============================================="
+echo "Creating dotfiles symlinks"
+echo "============================================="
+echo ""
+echo "---------------------------------------------"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir/
+
+    # Move existing files
+    if [ -h ~/.$file ]; then
+        echo "Moving ~/.$file to $backupdir/$file"
+        cp -L ~/.$file $backupdir/$file
+        rm ~/.$file
+    fi
+
+    # Create symlinks
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dotfilesdir/$file ~/.$file
+
+    echo "---------------------------------------------"
+
 done
 
 # Create symlinks for any color schemes as well
+echo ""
+echo "============================================="
+echo "Creating color symlinks"
+echo "============================================="
+echo ""
+echo "---------------------------------------------"
 for color in  $colors; do
 	mkdir -p ~/.vim/colors
-	echo "Creating symlink to $color in the colors directory of vim"
+	echo "Creating symlink from $color to ~/.vim/colors/$color"
 	ln -s $colorsdir/$color ~/.vim/colors/$color
+    echo "---------------------------------------------"
 done
