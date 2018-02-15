7a3dc13473dd454dce2cf32152df1ab7cd08d411
TreeTrum <sam@sjd.co>
2018-02-15 12:45:15 +1100

    Move vim config to a separate repo

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,13 +11,9 @@ backupdir="$dir/backup"		# old dotfiles backup directory
 colorsdir="$dir/vimcolors"
 dotfilesdir="$dir/dotfiles"
 
-files="eslintrc.yml sass-lint.yml vimrc"			# list of files/folders to symlink in homeidr
-colors="itg_flat.vim spacegray.vim"
+files="eslintrc.yml sass-lint.yml"			# list of files/folders to symlink in homeidr
 
 ########
-
-# Create create backup dir
-mkdir -p $backupdir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 echo ""
@@ -25,13 +21,12 @@ echo "============================================="
 echo "Creating dotfiles symlinks"
 echo "============================================="
 echo ""
-echo "---------------------------------------------"
 for file in $files; do
 
-    # Move existing files
+    echo "---------------------------------------------"
+
+    # Remove existing files
     if [ -h ~/.$file ]; then
-        echo "Moving ~/.$file to $backupdir/$file"
-        cp -L ~/.$file $backupdir/$file
         rm ~/.$file
     fi
 
@@ -41,24 +36,4 @@ for file in $files; do
 
     echo "---------------------------------------------"
 
-done
-
-# Create symlinks for any color schemes as well
-echo ""
-echo "============================================="
-echo "Creating color symlinks"
-echo "============================================="
-echo ""
-echo "---------------------------------------------"
-for color in  $colors; do
-	mkdir -p ~/.vim/colors
-
-    if [ -h ~/.vim/colors/$color ]; then
-        echo "Removing $color symlink"
-        rm ~/.vim/colors/$color
-    fi
-
-	echo "Creating symlink from $color to ~/.vim/colors/$color"
-	ln -s $colorsdir/$color ~/.vim/colors/$color
-    echo "---------------------------------------------"
 done
