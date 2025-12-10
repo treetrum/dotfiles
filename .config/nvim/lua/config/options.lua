d7d426a0b0bb19c5ca8cd5f4fb100e7e9a85d310
Sam Davis <sam@sjd.co>
2025-12-10 17:03:12 +1100

    enable prettier in nvim when .prettierrc file is found

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Only enable prettier when a .prettierrc file is detected
+vim.g.lazyvim_prettier_needs_config = true
