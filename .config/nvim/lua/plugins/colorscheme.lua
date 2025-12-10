50ebbc3a79cf333546a4dc8da33c20ccc4ba7a81
Sam Davis <sam@sjd.co>
2025-12-11 10:44:23 +1100

    explicitly load tokyonight theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,12 @@
 return {
+  -- Add theme
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- Configure LazyVim to use the theme
   { "LazyVim/LazyVim", opts = {
     colorscheme = "tokyonight",
   } },
